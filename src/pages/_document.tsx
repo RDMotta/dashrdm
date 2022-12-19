cead1919777b84caf53b7dcfa9cb0776d3cd3832
robson.motta <robson.motta@db1.com.br>
2022-12-19 16:34:01 -0300

    Add charts on dashboard

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,21 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-    render() {
-        return (
-            <Html>
-                <Head>
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link rel="stylesheets" href="https://fonts.googleapis.com/css2?family=Roboto:mght@400;500;700&display=swap" />                    
-                </Head>
-                <body>
-                    <Main/>
-                    <NextScript/>
-                </body>
-            </Html>
-            
-        )
-    }
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="stylesheets"
+            href="https://fonts.googleapis.com/css2?family=Roboto:mght@400;500;700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,14 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, SimpleGrid, Box, Text } from '@chakra-ui/react'
+import dynamic from 'next/dynamic';
 import { Header } from '../components/Header'
 import { Sidebar } from '../components/Sidebar';
 
+const Chart = dynamic(() => import('react-apexcharts'), { ssr:false })
+const options = {};
+
+const series = [
+ {name: 'id_series', data: [21,44,1,35,66,133,775]}
+];
 
 export default function Dashboard() {
     return(
@@ -9,7 +16,22 @@ export default function Dashboard() {
          <Header/>
          <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
             <Sidebar/>
-         </Flex>
+            <SimpleGrid flex="1" gap="4" minChildWidth="328px" >
+                <Box 
+                    p="8"
+                    bg="gray.800"
+                    borderRadius={8}>
+                    <Text fontSize="lg" mb="4">Inscritos</Text>
+                    <Chart options={options} series={series} type="area" height={160}/>
+                </Box>
+                <Box 
+                    p="8"
+                    bg="gray.800"
+                    borderRadius={8}>
+                    <Text fontSize="lg" mb="4">Abertura</Text>
+                </Box>
+            </SimpleGrid>
+         </Flex>         
       </Flex>
     );
 }
