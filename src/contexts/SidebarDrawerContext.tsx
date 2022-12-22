62a6c013d9ab189b125f3ceaa4ee1669833f054b
robson.motta <robson.motta@db1.com.br>
2022-12-21 22:51:43 -0300

    Added controller sidebar on router changed

--- src/contexts/SidebarDrawerContext.tsx
+++ src/contexts/SidebarDrawerContext.tsx
@@ -1,6 +1,7 @@
 
 import { useDisclosure, UseDisclosureReturn } from '@chakra-ui/hooks';
-import { ReactNode, createContext, useContext } from 'react';
+import { ReactNode, createContext, useContext, useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 interface SidebarDrawerProviderProps {
     children: ReactNode;
@@ -12,6 +13,11 @@ const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
 
 export function SidebarDrawerProvider({ children }: SidebarDrawerProviderProps) {
     const disclosure = useDisclosure()
+    const router = useRouter()
+
+    useEffect(() => {
+        disclosure.onClose()
+    }, [router.asPath])
     return(
         <SidebarDrawerContext.Provider value={disclosure} >
          {children}  
