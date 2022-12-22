a522511fd789a87a32a6e7a11df62594658054d0
robson.motta <robson.motta@db1.com.br>
2022-12-21 22:46:42 -0300

    Added responsive components

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,18 @@
 import { Flex, Box, Text, Avatar } from '@chakra-ui/react'
 
-export function Profile(){
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+export function Profile({ showProfileData }: ProfileProps){
     return (
         <Flex align="center">
+           { showProfileData && (
             <Box mr="4" textAlign="right">
                 <Text>Robson da Motta</Text>
                 <Text color="gray.300" fontSize="small">email@email.com</Text>        
             </Box>
+            ) } 
+            
             <Avatar size="md" name="Robson da Motta"/>
         </Flex>
     );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,21 @@
-import { Flex } from '@chakra-ui/react' 
-import { Logo } from './Logo';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react' 
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
+import { RiMenuLine } from 'react-icons/ri';
 
+
+import { Logo } from './Logo';
 import { NotificationNav } from './NotificationNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+    const { onOpen } = useSidebarDrawer()
+
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true,
+    })
+
     return(
         <Flex 
             as="header"
@@ -17,13 +27,26 @@ export function Header() {
             px="6"
             align="center"
         >
+            { !isWideVersion && (
+                <IconButton
+                    aria-label="Open menu drawer"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2"
+                >
+
+                </IconButton>
+            ) }
+
             <Logo />
             
-            <SearchBox />
+            { isWideVersion && <SearchBox /> }
             
             <Flex align="center" ml="auto">
               <NotificationNav />
-              <Profile /> 
+              <Profile showProfileData={isWideVersion}/> 
             </Flex>              
         </Flex>
     );
